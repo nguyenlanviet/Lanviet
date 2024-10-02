4961b0cbaa9284271c6969c3a5e05e6fdf858fd5
Việt Nguyễn <vietnl.ba12-193@st.usth.edu.vn>
2024-10-02 09:57:07 +0700

    Prac14

--- app/src/main/java/vn/edu/usth/myweatherapp/WeatherAndForecastFragment.java
+++ app/src/main/java/vn/edu/usth/myweatherapp/WeatherAndForecastFragment.java
@@ -1,2 +1,60 @@
-package vn.edu.usth.myweatherapp;public class WeatherAndForecastFragment {
+package vn.edu.usth.myweatherapp;
+
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WeatherAndForecastFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public WeatherAndForecastFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment WeatherAndForecastFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static WeatherAndForecastFragment newInstance(String param1, String param2) {
+        WeatherAndForecastFragment fragment = new WeatherAndForecastFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_weather_and_forecast, container, false);
+    }
 }
